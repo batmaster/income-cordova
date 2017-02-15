18120935d37bc8f0171527b377a9e49621fb899a
Poramate HomProkob <batmaster_kn@hotmail.com>
2017-02-15 20:44:58 +0700

    can plot graphs for user

--- db_connector.php
+++ db_connector.php
@@ -3,9 +3,9 @@
     mysql_select_db("income_cordova") or die(mysql_error());
     mysql_query("SET NAMES utf8");
 
-    // header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: *');
 
-    function sql($sql) {
+    function sql($sql, $asArray = true) {
         $result = mysql_query($sql) or die($sql . "\n" . mysql_error());
 
         logs($sql);
@@ -17,7 +17,7 @@
                 $rows[] = $r;
             }
 
-            if (count($rows) == 1) {
+            if (count($rows) == 1 && !$asArray) {
                 return $rows[0];
             }
 
@@ -39,6 +39,7 @@
     $function = $_POST["function"];
 
     if (isset($function)) {
+        /******************** #user ********************/
         if ($function == "check_user") {
             $username = $_POST["username"];
 
@@ -51,22 +52,56 @@
             $password = $_POST["password"];
             $email = $_POST["email"];
             $level = $_POST["level"];
+            $group = $_POST["group"];
             $phone = $_POST["phone"];
             $birthday = $_POST["birthday"];
 
-            sql("INSERT INTO user (username, password, name, lastname, email, level, phone, birthday, register_date)
-            VALUES ('$username', '$password', '$name', '$lastname', '$email', $level, '$phone', '$birthday', NOW())");
+            if ($level = 0) {
+                sql("INSERT INTO user (username, password, name, lastname, email, level, phone, birthday, register_date)
+                VALUES ('$username', '$password', '$name', '$lastname', '$email', $level, '$phone', '$birthday', NOW())");
+            }
+            else {
+                $code = "gl" . sql("SELECT LAST_INSERT_ID() + 1");
+                sql("INSERT INTO `group` (title, code) VALUES ('$group', '$code')");
 
-            echo json_encode(sql("SELECT id FROM user WHERE username = '$username' AND password = '$password'"));
+                $group_id = sql("SELECT id FROM `group` WHERE code = '$code'");
+
+                sql("INSERT INTO user (username, password, name, lastname, email, level, phone, birthday, register_date)
+                VALUES ('$username', '$password', '$name', '$lastname', '$email', $level, '$phone', '$birthday', NOW()), $group_id");
+            }
+
+            echo json_encode(sql("SELECT id, group_id FROM user WHERE username = '$username' AND password = '$password'"));
         }
         else if ($function == "login") {
             $username = $_POST["username"];
             $password = $_POST["password"];
 
-            echo json_encode(sql("SELECT id FROM user WHERE username = '$username' AND password = '$password'"));
+            echo json_encode(sql("SELECT id, level, group_id FROM user WHERE username = '$username' AND password = '$password'", false));
         }
-        else if ($function == "get_transaction_titles") { // ยังไม่ได้ใช้
+        else if ($function == "get_members_by_group_id") {
+            $group_id = $_POST["group_id"];
 
+            echo json_encode(sql("SELECT id, CONCAT(name, ' ', lastname) name, email, phone, group_id FROM user WHERE group_id = $group_id UNION
+            SELECT id, CONCAT(name, ' ', lastname) name, email, phone, group_id FROM user WHERE group_id = (-1 * $group_id)"));
+        }
+        else if ($function == "get_group_detail_by_head_user_id") {
+            $user_id = $_POST["user_id"];
+
+            echo json_encode(sql("SELECT g.title, g.code, (SELECT COUNT(*) FROM user WHERE group_id = -1 * g.id) pending FROM `group` g, user u WHERE g.id = u.group_id AND u.id = $user_id", false));
+        }
+        else if ($function == "approve_member") {
+            $user_id = $_POST["user_id"];
+
+            sql("UPDATE user SET group_id = (-1 * group_id) WHERE id = $user_id");
+            sql("UPDATE transaction SET group_id = (SELECT group_id FROM user WHERE id = $user_id) WHERE user_id = $user_id");
+            echo json_error(sql("SELECT COUNT(*) ok FROM user WHERE id = $user_id AND group_id > 0"));
+        }
+        /******************** #transaction ********************/
+        else if ($function == "get_transaction_titles") {
+            $user_id = $_POST["user_id"];
+            $type = $_POST["type"];
+
+            echo json_encode(sql("SELECT DISTINCT(title) FROM transaction WHERE user_id = $user_id AND type = $type"));
         }
         else if ($function == "add_transaction") {
             $user_id = $_POST["user_id"];
@@ -74,17 +109,40 @@
             $title = $_POST["title"];
             $amount = $_POST["amount"];
             $date = $_POST["date"];
+            $group_id = $_POST["group_id"];
 
             sql("INSERT INTO transaction (user_id, type, title, amount, date, group_id)
-            VALUES ($user_id, $type, '$title', $amount, '$date', (SELECT group_id FROM user WHERE id = $user_id))");
+            VALUES ($user_id, $type, '$title', $amount, '$date', $group_id)");
 
-            echo json_encode(sql("SELECT id FROM transaction WHERE user_id = $user_id AND type = $type AND title = '$title' AND amount = $amount AND date = '$date'"));
+            echo json_encode(sql("SELECT id FROM transaction WHERE user_id = $user_id AND type = $type AND title = '$title' AND amount = $amount AND date = '$date'", false));
         }
-        else if ($function == "get_transactions_by_user_id") { // ยังไม่ได้ใช้
+        else if ($function == "get_transactions_by_user_id") {
             $user_id = $_POST["user_id"];
             $date_from = $_POST["date_from"];
             $date_to = $_POST["date_to"];
 
+            echo json_encode(sql("SELECT t.type, t.title, t.amount, t.date FROM transaction t, user u
+                WHERE t.user_id = u.id AND '$date_from' <= DATE(t.date) AND DATE(t.date) <= '$date_to' AND u.id = $user_id
+                ORDER BY DATE(t.date)"));
+        }
+        else if ($function == "get_transactions_by_title_by_user_id") {
+            $user_id = $_POST["user_id"];
+            $date_from = $_POST["date_from"];
+            $date_to = $_POST["date_to"];
+
+            echo json_encode(sql("SELECT t.type, t.title, SUM(t.amount) amount, COUNT(*) count FROM transaction t, user u
+                WHERE t.user_id = u.id AND '$date_from' <= DATE(t.date) AND DATE(t.date) <= '$date_to' AND u.id = $user_id
+                GROUP BY type, title ORDER BY amount DESC"));
+        }
+        else if ($function == "get_transactions_group_by_date_by_user_id") {
+            $user_id = $_POST["user_id"];
+            $date_from = $_POST["date_from"];
+            $date_to = $_POST["date_to"];
+
+            echo json_encode(sql("SELECT DATE(t.date) date, SUM(if(type = 0, t.amount, 0)) income, SUM(if(type = 0, 1, 0)) count_income, SUM(if(type = 1, t.amount, 0)) outcome, SUM(if(type = 1, 1, 0)) count_outcome
+                FROM transaction t, user u
+                WHERE t.user_id = u.id AND '$date_from' <= DATE(t.date) AND DATE(t.date) <= '$date_to' AND u.id = $user_id
+                GROUP BY DATE(t.date)"));
         }
         else if ($function == "get_transactions_by_group_id") {
             $date_from = $_POST["date_from"];
@@ -92,8 +150,25 @@
             $group_id = $_POST["group_id"];
 
             echo json_encode(sql("SELECT t.type, t.title, t.amount, t.date, CONCAT(u.name, ' ', u.lastname) name FROM transaction t, user u
-                WHERE t.user_id = u.id AND '$date_from' <= DATE(t.date) AND DATE(t.date) <= '$date_to' AND t.group_id = $group_id"));
+                WHERE t.user_id = u.id AND '$date_from' <= DATE(t.date) AND DATE(t.date) <= '$date_to' AND u.group_id = $group_id
+                ORDER BY DATE(t.date)"));
         }
+        /******************** #group ********************/
+        else if ($function == "get_group") {
+            $group_id = $_POST["group_id"];
 
+            echo json_encode(sql("SELECT id, title, code FROM `group` WHERE id = $group_id", false));
+        }
+        else if ($function == "get_groups") {
+
+            echo json_encode(sql("SELECT id, title, code FROM `group`"));
+        }
+        else if ($function == "join_group") {
+            $user_id = $_POST["user_id"];
+            $group_id = $_POST["group_id"];
+
+            echo sql("UPDATE user SET group_id = $group_id WHERE id = $user_id");
+            echo json_encode(sql("SELECT COUNT(*) ok FROM user WHERE id = $user_id AND group_id = $group_id", false));
+        }
     }
 ?>
--- platforms/android/assets/www/js/page5.js
+++ platforms/android/assets/www/js/page5.js
@@ -1,36 +1,45 @@
 $(document).ready(function() {
+
+    // f 1
     $(document).on("pagebeforeshow", "#pagefive", function() {
-        $("#pagefive #date-from").datebox('setTheDate', new Date());
-        $("#pagefive #date-to").datebox('setTheDate', new Date());
+        $("#pagefive #fragment-1 #date-from").datebox('setTheDate', new Date());
+        $("#pagefive #fragment-1 #date-to").datebox('setTheDate', new Date());
+
+        google.charts.load('current', {'packages':['corechart']});
     });
 
-    $(document).on('click','#pagefive #date-from', function() {
-        $("#pagefive #date-from").datebox('setTheDate', new Date());
+    $(document).on('click','#pagefive #fragment-1 #date-from', function() {
+        $("#pagefive #fragment-1 #date-from").datebox('setTheDate', new Date());
     });
 
-    $(document).on("change", "#pagefive #date-from", function() {
-        getTransactions();
+    $(document).on("change", "#pagefive #fragment-1 #date-from", function() {
+        getTransactionsTable();
+        getDailyBar();
+        getDailyPie();
     });
 
-    $(document).on('click','#pagefive #date-to', function() {
-        $("#pagefive #date-to").datebox('setTheDate', new Date());
+    $(document).on('click','#pagefive #fragment-1 #date-to', function() {
+        $("#pagefive #fragment-1 #date-to").datebox('setTheDate', new Date());
     });
 
-    $(document).on("change", "#pagefive #date-to", function() {
-        getTransactions();
+    $(document).on("change", "#pagefive #fragment-1 #date-to", function() {
+        getTransactionsTable();
+        getDailyBar();
+        getDailyPie();
     });
 
-    $(document).on('click','#pagefive #today', function() {
-        $("#pagefive #date-from").datebox('setTheDate', new Date());
-        $("#pagefive #date-to").datebox('setTheDate', new Date());
+    // f 1 1
+    $(document).on('click','#pagefive #fragment-1 #fragment-1 #today', function() {
+        $("#pagefive #fragment-1 #date-from").datebox('setTheDate', new Date());
+        $("#pagefive #fragment-1 #date-to").datebox('setTheDate', new Date());
     });
 
-    function getTransactions() {
-        $("#pagefive #table-body").empty();
+    function getTransactionsTable() {
+        $("#pagefive #fragment-1 #fragment-1 #table-body").empty();
 
-        var df = $("#pagefive #date-from").datebox('getTheDate');
+        var df = $("#pagefive #fragment-1 #date-from").datebox('getTheDate');
         var date_from = TODATE(df.getFullYear(), df.getMonth() + 1, df.getDate());
-        var dt = $("#pagefive #date-to").datebox('getTheDate');
+        var dt = $("#pagefive #fragment-1 #date-to").datebox('getTheDate');
         var date_to = TODATE(dt.getFullYear(), dt.getMonth() + 1, dt.getDate(), 23, 59, 59);
 
         $.ajax({
@@ -44,13 +53,12 @@ $(document).ready(function() {
                 "user_id": localStorage.getItem(KEY_USERID)
             }
         }).done(function(response) {
-            $("#pagefive #table-body").empty();
-            console.log("getting transaction ok " + response);
+            $("#pagefive #fragment-1 #fragment-1 #table-body").empty();
             if (response != undefined) {
                 for (var i = 0; i < response.length; i++) {
                     var r = response[i];
                     console.log(TYPE[r.type]);
-                    $("#pagefive #table-body").append('\
+                    $("#pagefive #fragment-1 #fragment-1 #table-body").append('\
                     <tr>\
                         <th data-colstart="1">' + (i+1) + '</th>\
                         <td data-colstart="2">' + r.title + '</td>\
@@ -70,8 +78,8 @@ $(document).ready(function() {
                     </tr>');
                 }
 
-                $('#pagefive #table-column-toggle').trigger("create");
-                $('#pagefive #table-column-toggle').table("refresh");
+                $('#pagefive #fragment-1 #fragment-1 #table-column-toggle').trigger("create");
+                $('#pagefive #fragment-1 #fragment-1 #table-column-toggle').table("refresh");
             }
             else {
                 console.log("getting transaction failed");
@@ -80,5 +88,148 @@ $(document).ready(function() {
                     console.log(jqXHR + "\n" + textStatus + "\n" + errorThrown);
         });
     }
+
+    // f 1 2
+        function getDailyBar() {
+            var df = $("#pagefive #fragment-1 #date-from").datebox('getTheDate');
+            var date_from = TODATE(df.getFullYear(), df.getMonth() + 1, df.getDate());
+            var dt = $("#pagefive #fragment-1 #date-to").datebox('getTheDate');
+            var date_to = TODATE(dt.getFullYear(), dt.getMonth() + 1, dt.getDate(), 23, 59, 59);
+
+            console.log("from " + date_from + "   to " + date_to);
+
+            $.ajax({
+                url: SERVER_URL,
+                type: "POST",
+                dataType: "json",
+                data: {
+                    "function": "get_transactions_group_by_date_by_user_id",
+                    "date_from": date_from,
+                    "date_to": date_to,
+                    "user_id": localStorage.getItem(KEY_USERID)
+                }
+            }).done(function(response) {
+                console.log(JSON.stringify(response));
+                google.charts.setOnLoadCallback(function() {
+                    var data = new google.visualization.DataTable();
+
+                    data.addColumn('date', 'วันที่');
+//        data.addColumn('string', 'จำนวนรายการ')
+                    data.addColumn('number', 'รายรับ');
+                    data.addColumn('number', 'รายจ่าย');
+
+//        df.setDate(df.getDate() + 1);
+//        dt.setDate(dt.getDate() + 1);
+                    console.log("1111")
+                    for (var d = df; d <= dt; d.setDate(d.getDate() + 1)) {
+                        var added = false;
+                        for (var i = 0; i < response.length; i++) {
+                            var r = response[i];
+
+                            var current = new Date(r.date);
+                            current.setHours(0);
+                            console.log(d + " " + current + " " + d.getTime() + " " + current.getTime());
+
+                            if (d.getTime() == current.getTime()) {
+                                data.addRow([new Date(d.getTime()), Number(r.income), Number(r.outcome)]);
+                                response.splice(i, 1);
+                                added = true;
+                                break;
+                            }
+                            console.log("2222")
+                        }
+                        if (!added) {
+                            data.addRow([new Date(d.getTime()), 0, 0]);
+                        }
+                    }
+                    console.log("3333")
+
+                    var div = $("#pagefive #fragment-1 #fragment-2 #bar_div");
+                    var chartwidth = div.width();
+
+                    var options = {
+                      title: 'กราฟแสดงรายรับ-รายจ่ายรายวัน',
+                      width: chartwidth,
+                      height: 400,
+                      hAxis: {
+                        format: 'yyyy-MM-dd',
+                        gridlines: {count: 15},
+                        slantedText: true,
+                        slantedTextAngle: 90,
+//                        minValue: new Date("2017-01-01")
+                      },
+                      vAxis: {
+                        gridlines: {color: 'none'},
+                        minValue: 0,
+                      },
+                      bar: {groupWidth: 20},
+                      explorer: {actions: ['dragToPan', 'rightClickToReset'], axis: 'horizontal'}
+                    };
+
+                    console.log(data);
+                    var chart = new google.visualization.ColumnChart(document.getElementById('bar_div'));
+                    chart.draw(data, options);
+                });
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                        console.log(jqXHR + "\n" + textStatus + "\n" + errorThrown);
+            });
+    }
+
+    // f 1 3
+    function getDailyPie() {
+        var df = $("#pagefive #fragment-1 #date-from").datebox('getTheDate');
+        var date_from = TODATE(df.getFullYear(), df.getMonth() + 1, df.getDate());
+        var dt = $("#pagefive #fragment-1 #date-to").datebox('getTheDate');
+        var date_to = TODATE(dt.getFullYear(), dt.getMonth() + 1, dt.getDate(), 23, 59, 59);
+
+        $.ajax({
+            url: SERVER_URL,
+            type: "POST",
+            dataType: "json",
+            data: {
+                "function": "get_transactions_by_title_by_user_id",
+                "date_from": date_from,
+                "date_to": date_to,
+                "user_id": localStorage.getItem(KEY_USERID)
+            }
+        }).done(function(response) {
+            google.charts.setOnLoadCallback(function() {
+                var income = new google.visualization.DataTable();
+                income.addColumn('string', 'รายการ');
+                income.addColumn('number', 'จำนวน');
+
+                var outcome = new google.visualization.DataTable();
+                outcome.addColumn('string', 'รายการ');
+                outcome.addColumn('number', 'จำนวน');
+
+                for (var i = 0; i < response.length; i++) {
+                    var r = response[i];
+                    if (r.type == "0") {
+                        income.addRow([r.title + " (" + r.count + ")", Number(r.amount)]);
+                    }
+                    else {
+                        outcome.addRow([r.title + " (" + r.count + ")", Number(r.amount)]);
+                    }
+                }
+
+                var div = $("#pagefive #fragment-1 #fragment-3 #income_div");
+                var chartwidth = div.width();
+
+                var incomeOption = {'title':'รายรับ', 'width':chartwidth, 'height':300};
+                var outcomeOption = {'title':'รายจ่าย', 'width':chartwidth, 'height':300};
+
+                new google.visualization.PieChart(document.getElementById('income_div')).draw(income, incomeOption);
+                new google.visualization.PieChart(document.getElementById('outcome_div')).draw(outcome, outcomeOption);
+            });
+
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR + "\n" + textStatus + "\n" + errorThrown);
+        });
+    }
+
+    $(document).on("pageshow", "#pagefive", function() {
+        getDailyBar();
+        getDailyPie();
+    });
 
 });
